cafeabdef9d4579137c369b73f3b149b5677a0e1
Drew Tayman <dretay@gmail.com>
2020-08-04 10:17:57 -0400

    adding ugfx integration

---
+++ lib/ugfx-config-uc1698/src/ugfx-config-uc1698.h
@@ -0,0 +1,7 @@
+#ifndef UC1698_UGFX_CONFIG_H
+#define UC1698_UGFX_CONFIG_H
+
+#include <gfx.h>  // and use gfx.h in that directory
+#include <ugfx-arduino-gdisp-uc1698.h> // display driver library
+
+#endif // UC1698_UGFX_CONFIG_H
